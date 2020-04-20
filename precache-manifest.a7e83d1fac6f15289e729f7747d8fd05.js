9611d668492a58c37ea2e20d60278bf9cffda6c2
neena <neena@Prasads-MacBook-Pro.local>
2020-04-20 10:08:47 -0400

    Updates

---
+++ precache-manifest.a7e83d1fac6f15289e729f7747d8fd05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90a7b9c484c4ebfbe95eb75d3d40f04f",
+    "url": "/NeenaDev/MarketTestApp.git/index.html"
+  },
+  {
+    "revision": "e2a1ded60cbc2abb1c4d",
+    "url": "/NeenaDev/MarketTestApp.git/static/css/main.1dfaac36.chunk.css"
+  },
+  {
+    "revision": "af53ca84085653144e5b",
+    "url": "/NeenaDev/MarketTestApp.git/static/js/2.7e919d6d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/NeenaDev/MarketTestApp.git/static/js/2.7e919d6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2a1ded60cbc2abb1c4d",
+    "url": "/NeenaDev/MarketTestApp.git/static/js/main.3b21413e.chunk.js"
+  },
+  {
+    "revision": "4cc53c0761f6279cca17",
+    "url": "/NeenaDev/MarketTestApp.git/static/js/runtime-main.6c34145f.js"
+  }
+]);
